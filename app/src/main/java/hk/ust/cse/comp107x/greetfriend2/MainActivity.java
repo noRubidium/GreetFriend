0b7341fd06f35d4591e9388a0cb3ec6e8853fb36
noRubidium <markshen5295@hotmail.com>
2015-08-05 22:20:37 +1000

    Initial commit

---
+++ app/src/main/java/hk/ust/cse/comp107x/greetfriend2/MainActivity.java
@@ -0,0 +1,62 @@
+package hk.ust.cse.comp107x.greetfriend2;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onClick(View v){
+
+
+    }
+
+    public void buttonClick(View view) {
+
+        EditText edit = (EditText) findViewById(R.id.input);
+        String greetMessage = "You are"+ edit.getText().toString();
+
+//        Toast.makeText(this,getString(R.string.dser),Toast.LENGTH_SHORT).show();
+                Intent in = new Intent(this,showMessage.class);
+
+                //textMessage.setText(greetMessage);
+                in.putExtra("Message","Hi！"+greetMessage);
+                startActivity(in);
+
+    }
+}
